851bf44d8276317ae85484a540dda548d01ac886
JonasXPX <jonasdefarias@hotmail.com>
2020-10-24 20:52:44 -0300

    close #3 adicionado lombok, com ObjectMapper e testes

--- src/main/java/br/com/gritto/gritto4/domain/Usuario.java
+++ src/main/java/br/com/gritto/gritto4/domain/Usuario.java
@@ -1,158 +1,46 @@
 package br.com.gritto.gritto4.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Usuario implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "nome")
-	private String nome;
-	
-	@Column(name = "rg")
-	private String rg;
-	
-	@Column(name = "cpf")
-	private String cpf;
-	
-	@Column(name = "dataNascimento")
-	private String dataNascimento;
-	
-	@Column(name = "telefone")
-	private String telefone;
-	
-	@Column(name = "senha")
-	private String senha;
-	
-	@Column(name = "email")
-	private String email;
-	
-	@Column(name = "sexo")
-	private String sexo;	
-	
-	public Usuario() {		
-	}
+  private static final long serialVersionUID = 1L;
 
-	public Usuario(Long id, String nome, String rg, String cpf, String dataNascimento, String telefone, String senha,
-			String email, String sexo) {
-		super();
-		
-		this.id = id;
-		this.nome = nome;
-		this.rg = rg;
-		this.cpf = cpf;
-		this.dataNascimento = dataNascimento;
-		this.telefone = telefone;
-		this.senha = senha;
-		this.email = email;
-		this.sexo = sexo;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	public Long getId() {
-		return id;
-	}
+  @Column(name = "nome")
+  private String nome;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  @Column(name = "rg")
+  private String rg;
 
-	public String getNome() {
-		return nome;
-	}
+  @Column(name = "cpf")
+  private String cpf;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+  @Column(name = "dataNascimento")
+  private String dataNascimento;
 
-	public String getRg() {
-		return rg;
-	}
+  @Column(name = "telefone")
+  private String telefone;
 
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
+  @Column(name = "senha")
+  private String senha;
 
-	public String getCpf() {
-		return cpf;
-	}
+  @Column(name = "email")
+  private String email;
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+  @Column(name = "sexo")
+  private String sexo;
 
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}		
 }
