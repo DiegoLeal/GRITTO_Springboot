80536cb56cdad6b92363e5106378f6f8be11bc7a
JonasXPX <jonasdefarias@hotmail.com>
2020-11-25 07:19:16 -0300

    no ref. continuacao do desenvolvimento

--- src/main/java/br/com/gritto/security/WebSecurityConfig.java
+++ src/main/java/br/com/gritto/security/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package br.com.gritto.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -16,38 +18,47 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/usuarios").permitAll()
-			.antMatchers(HttpMethod.POST, "/usuarios").permitAll()
-			.antMatchers(HttpMethod.GET, "/usuarios/{usuarioId}").permitAll()
-			.antMatchers(HttpMethod.PUT, "usuarios/{usuarioId}").permitAll()
-			.antMatchers(HttpMethod.DELETE, "usuarios/{usuarioId}").permitAll()
-			.antMatchers(HttpMethod.GET, "/v2/api-docs").permitAll()
-			.antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
-			.antMatchers(HttpMethod.GET, "/webjars/**").permitAll()
-			.antMatchers(HttpMethod.GET, "/swagger-resources/**").permitAll()
-			.antMatchers(HttpMethod.POST, "/login").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			
-			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
-	                UsernamePasswordAuthenticationFilter.class)
-			
-			.addFilterBefore(new JWTAuthenticationFilter(),
-	                UsernamePasswordAuthenticationFilter.class);
-	}
-	
-	@Bean
-	@Autowired
-    public UserDetailsService userDetailsService() {
+	private UserServiceImpl userService;
+	private PasswordEncoder encoder;
 
-        @SuppressWarnings("deprecation")
-		User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf().disable().authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/usuarios").permitAll()
+            .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
+            .antMatchers(HttpMethod.GET, "/usuarios/{usuarioId}").permitAll()
+            .antMatchers(HttpMethod.PUT, "usuarios/{usuarioId}").permitAll()
+            .antMatchers(HttpMethod.DELETE, "usuarios/{usuarioId}").permitAll()
+            .antMatchers(HttpMethod.GET, "/v2/api-docs").permitAll()
+            .antMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
+            .antMatchers(HttpMethod.GET, "/webjars/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/swagger-resources/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+
+            .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                    UsernamePasswordAuthenticationFilter.class)
+
+            .addFilterBefore(new JWTAuthenticationFilter(),
+                    UsernamePasswordAuthenticationFilter.class);
+  }
+
+  @Bean
+  public DaoAuthenticationProvider authProvider() {
+  	DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+  	authProvider.setUserDetailsService(userService);
+  	authProvider.setPasswordEncoder(encoder);
+  	return authProvider;
 	}
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    @SuppressWarnings("deprecation")
+    User.UserBuilder users = User.withDefaultPasswordEncoder();
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(users.username("user").password("password").roles("USER").build());
+    manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+    return manager;
+  }
 }
