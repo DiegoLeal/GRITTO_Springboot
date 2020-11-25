1795a56345acc4b5379e410ab5155fd0b37ebaa5
Jonas Peretiatko <jonas.peretiatko@desbravador.com.br>
2020-11-25 15:06:35 -0300

    no ref. adicionado README

--- src/main/java/br/com/gritto/security/WebSecurityConfig.java
+++ src/main/java/br/com/gritto/security/WebSecurityConfig.java
@@ -9,10 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -26,12 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/usuarios").permitAll()
-                .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
-                .antMatchers(HttpMethod.GET, "/usuarios/{usuarioId}").permitAll()
-                .antMatchers(HttpMethod.PUT, "usuarios/{usuarioId}").permitAll()
-                .antMatchers(HttpMethod.DELETE, "usuarios/{usuarioId}").permitAll()
-
+            .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
             .antMatchers(HttpMethod.POST, "/login").permitAll()
             .anyRequest().authenticated()
             .and()
